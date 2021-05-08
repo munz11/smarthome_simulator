db1264a63ff51609ae6c5584d5f4c813965265c6
munz11 <muna.azam@hotmail.com>
2021-05-08 15:24:47 +0200

    Home page restrictions work

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,7 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
+          <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
           <b-nav-item :to="{ name: 'FloorPlan' }">FloorPlan</b-nav-item>
           <b-nav-item :to="{ name: 'Sensors' }">Sensors</b-nav-item>
           <b-nav-item :to="{ name: 'Simulation' }">Simulation</b-nav-item>

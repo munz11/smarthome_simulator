f6501d43b22cd4d68a20d4d2751032315063a5de
munz11 <muna.azam@hotmail.com>
2021-05-16 14:34:39 +0200

    Can download the floorplan

--- src/components/widgets/uploadDownload.vue
+++ src/components/widgets/uploadDownload.vue
@@ -7,7 +7,8 @@
         <v-btn text @click="close"> &times; </v-btn>
       </v-card-title>
       <v-card-text>
-        <v-btn outlined rounded text @click="upload"> Upload JSON file </v-btn>
+        <v-btn outlined rounded text @click="upload"> Upload JSON</v-btn>
+        <br>
         <v-btn outlined rounded text @click="download"> Download</v-btn>
       </v-card-text>
     </v-card>
@@ -15,13 +16,14 @@
 </template>
 
 <script>
+import exportFromJSON from "export-from-json";
 export default {
   name: "UploadDownload",
   data() {
-      return{
-          passiveSensors:[],
-          activeSensors:[],
-      }
+    return {
+      passiveSensors: [],
+      activeSensors: [],
+    };
   },
   methods: {
     close() {
@@ -31,30 +33,31 @@ export default {
       console.log("upload a filee");
     },
     download() {
-        this.getPassiveActiveSensors();
-        let jsonData = {
-            tileSideLength:this.$store.state.floorPlanDetails.tileSideLenght,
-            width:this.$store.state.floorPlanDetails.width,
-            height:this.$store.state.floorPlanDetails.height,
-            agent:{position:this.$store.state.agent,speed:1},
-            passiveSensors:this.passiveSensors,
-            activeSensors:this.activeSensors,
-            walls:this.$store.state.walls,
-            entities: this.$store.state.entities
-        },
-        
-
+      this.getPassiveActiveSensors();
+      let data = {
+        tileSideLength: this.$store.state.floorPlanDetails.tileSideLenght,
+        width: this.$store.state.floorPlanDetails.width,
+        height: this.$store.state.floorPlanDetails.height,
+        agent: { position: this.$store.state.agent, speed: 1 },
+        passiveSensors: this.passiveSensors,
+        activeSensors: this.activeSensors,
+        walls: this.$store.state.walls,
+        entities: this.$store.state.entities,
+      };
+      const fileName = "floorPlan";
+      const exportType = "json";
+      exportFromJSON({ data, fileName, exportType });
     },
-    getPassiveActiveSensors(){
-        let sensors = this.$store.state.sensors;
-        for(let i=0;i<sensors.length;i++){
-            if(sensors[i].isPassive()){
-                this.passiveSensors.push(sensors[i].getPassiveSensor());
-            }else{
-                this.activeSensors.push(sensors[i].getActiveSensor());
-            }
+    getPassiveActiveSensors() {
+      let sensors = this.$store.state.sensors;
+      for (let i = 0; i < sensors.length; i++) {
+        if (sensors[i].isPassive()) {
+          this.passiveSensors.push(sensors[i].getPassiveSensor());
+        } else {
+          this.activeSensors.push(sensors[i].getActiveSensor());
         }
-    }
+      }
+    },
   },
 };
 </script>

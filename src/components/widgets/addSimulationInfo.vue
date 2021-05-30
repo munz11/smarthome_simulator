82fdc36a9430e35a7d64334d868ee5fe8bed41ce
munz11 <muna.azam@hotmail.com>
2021-05-30 19:44:09 +0200

    The server info is shown to the frontend in realtime, so as the simulation runs, the output is displayed

--- src/components/widgets/addSimulationInfo.vue
+++ src/components/widgets/addSimulationInfo.vue
@@ -1,105 +1,122 @@
 <template>
   <div>
-    <v-card class="mx-auto" max-width="500" outlined elevation="2" shaped>
-      <v-card-title>Please enter details for the simulation</v-card-title>
-      <v-card-text>
-        <v-form v-model="isValid">
-          <v-row>
-            <v-col>
-              <v-text-field
-                label="Date"
-                v-model="date"
-                required
-                :rules="[
-                  (v) => !!v || 'Required',
-                  (v) =>
-                    /^\d{4}-\d{2}-\d{2}$/.test(v) ||
-                    'Enter Date in the correct format: yyyy-mm-dd',
-                ]"
-              ></v-text-field>
-            </v-col>
-            <v-col>
-              <v-text-field
-                label="Time"
-                v-model="time"
-                required
-                hint="HH:mm:ss"
-                :rules="[
-                  (v) => !!v || 'Required',
-                  (v) =>
-                    /^\d{2}:\d{2}:\d{2}$/.test(v) ||
-                    'Enter time in the correct format: HH:mm:ss',
-                ]"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-checkbox
-            v-model="instantSimulation"
-            label="Instant Simulation"
-          ></v-checkbox>
-          <v-text-field
-            label="Relative Time"
-            v-model="relativeTime"
-            required
-            :rules="[
-              (v) => !!v || 'Required',
-              (v) => /^\d*$/.test(v) || 'Enter a number',
-            ]"
-          ></v-text-field>
-          <v-text-field
-            label="Seed"
-            v-model="seed"
-            required
-            hint="156241"
-            :rules="[
-              (v) => !!v || 'Required',
-              (v) => /^\d*$/.test(v) || 'Enter a number',
-            ]"
-          ></v-text-field>
-          <v-checkbox v-model="mqttOutput" label="Mqtt Output"></v-checkbox>
-          <v-row>
-            <v-col>
-              <v-text-field
-                label="Mqtt Host"
-                v-model="mqttHost"
-                required
-                :rules="[(v) => !!v || 'Required']"
-              ></v-text-field>
-            </v-col>
-            <v-col>
-              <v-text-field
-                label="Mqtt Port"
-                v-model="mqttPort"
-                required
-                :rules="[(v) => !!v || 'Required']"
-              ></v-text-field>
-            </v-col>
-            <v-col>
-              <v-text-field
-                label="Root Topic"
-                v-model="rootTopic"
-                required
-                :rules="[(v) => !!v || 'Required']"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-form>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn outlined rounded text :disabled="!isValid" @click="submit">
-          Submit</v-btn
+    <v-card
+      class="mx-auto"
+      width="500"
+      height="550"
+      outlined
+      elevation="2"
+      shaped
+    >
+      <div v-if="infoCard">
+        <v-card-title
+          >Please enter details for the simulation
+          <v-btn text @click="close"> &times; </v-btn></v-card-title
         >
-      </v-card-actions>
+        <v-card-text>
+          <v-form v-model="isValid">
+            <v-row>
+              <v-col>
+                <v-text-field
+                  label="Date"
+                  v-model="date"
+                  required
+                  :rules="[
+                    (v) => !!v || 'Required',
+                    (v) =>
+                      /^\d{4}-\d{2}-\d{2}$/.test(v) ||
+                      'Enter Date in the correct format: yyyy-mm-dd',
+                  ]"
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  label="Time"
+                  v-model="time"
+                  required
+                  hint="HH:mm:ss"
+                  :rules="[
+                    (v) => !!v || 'Required',
+                    (v) =>
+                      /^\d{2}:\d{2}:\d{2}$/.test(v) ||
+                      'Enter time in the correct format: HH:mm:ss',
+                  ]"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-checkbox
+              v-model="instantSimulation"
+              label="Instant Simulation"
+            ></v-checkbox>
+            <v-text-field
+              label="Relative Time"
+              v-model="relativeTime"
+              required
+              :rules="[
+                (v) => !!v || 'Required',
+                (v) => /^\d*$/.test(v) || 'Enter a number',
+              ]"
+            ></v-text-field>
+            <v-text-field
+              label="Seed"
+              v-model="seed"
+              required
+              hint="156241"
+              :rules="[
+                (v) => !!v || 'Required',
+                (v) => /^\d*$/.test(v) || 'Enter a number',
+              ]"
+            ></v-text-field>
+            <v-checkbox v-model="mqttOutput" label="Mqtt Output"></v-checkbox>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  label="Mqtt Host"
+                  v-model="mqttHost"
+                  required
+                  :rules="[(v) => !!v || 'Required']"
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  label="Mqtt Port"
+                  v-model="mqttPort"
+                  required
+                  :rules="[(v) => !!v || 'Required']"
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  label="Root Topic"
+                  v-model="rootTopic"
+                  required
+                  :rules="[(v) => !!v || 'Required']"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn outlined rounded text :disabled="!isValid" @click="submit">
+            Submit</v-btn
+          >
+        </v-card-actions>
+      </div>
+      <div v-if="!infoCard">
+        <v-card-title
+          >Server output
+          <v-btn text @click="close"> &times; </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <b-form-textarea
+            :value="simulationInfo"
+            plaintext
+            rows="18"
+            max-rows="18"
+          ></b-form-textarea>
+        </v-card-text>
+      </div>
     </v-card>
-    <v-snackbar v-model="SnackBar" timeout="-1">
-      {{ text }}
-
-      <template v-slot:action="{ attrs }">
-        <v-btn color="pink" text v-bind="attrs" @click="closeSnackBar">
-          {{ btnText }}
-        </v-btn>
-      </template>
-    </v-snackbar>
   </div>
 </template>
 
@@ -112,23 +129,22 @@ export default {
   data: () => {
     return {
       date: "",
-      time:"",
+      time: "",
       instantSimulation: false,
       relativeTime: 1,
       mqttOutput: false,
       mqttHost: "broker.hivemq.com",
       mqttPort: "1883",
       rootTopic: "smartHome",
-      text: "",
-      btnText: "",
-      SnackBar: false,
       isValid: true,
-      seed: null,
-      simulationInfo:"",
+      seed: 0,
+      simulationInfo: "",
+      infoCard: true,
     };
   },
   methods: {
     submit() {
+      this.infoCard = false;
       let dateObject = new Date();
       let datearray = this.date.split("-");
       let timeobject = this.time.split(":");
@@ -160,35 +176,21 @@ export default {
           axios
             .post(this.$smartHomeBackend.getUrlSimulation(), simulationJson)
             .then(() => {
-              if (this.mqttOutput) {
-                this.text =
-                  "The simulation has started, the data can be found at the mqtt host:" +
-                  this.mqttHost+"\n"+this.simulationInfo;
-              } else {
-                this.text = this.simulationInfo;
-              }
-              this.btnText = "Close";
-              this.SnackBar = true;
               this.stompClient.disconnect();
             })
             .catch((err) => {
-              this.text = err;
-              this.btnText = "Close";
-              this.SnackBar = true;
+              this.simulationInfo = err;
             });
           this.stompClient.subscribe("/SimulationStatus", (message) => {
-            this.simulationInfo=this.simulationInfo+message.body;
+            this.simulationInfo = this.simulationInfo + "\n" + message.body;
           });
         },
         (error) => {
-          console.log(error);
+          this.simulationInfo = error;
         }
       );
     },
-    closeSnackBar() {
-      this.text = "";
-      this.btnText = "";
-      this.SnackBar = false;
+    close() {
       this.$emit("simulationClose");
     },
   },
